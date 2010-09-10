ebcda3e53b7234e7d2fe441f5d3c9d3ae7fb0e66
JC Grubbs <grubbs.jc@gmail.com>
2010-09-10 15:25:37 -0500

    documentation

--- NoRMatic/NoRMaticConfig.cs
+++ NoRMatic/NoRMaticConfig.cs
@@ -25,10 +25,21 @@ namespace NoRMatic {
             }
         }
 
+        /// <summary>
+        /// In order for user auditing to work, an anonymous function will need to be supplied as the current user provider.
+        /// This function will simply return a string for the current user.  This provider pattern allows for the method
+        /// which looks up the current user to be changed at runtime.
+        /// </summary>
         public static void SetCurrentUserProvider(Func<string> provider) {
             ConfigContainer.Instance.CurrentUserProvider = provider;
         }
 
+        /// <summary>
+        /// By default, NoRMatic will look for a connection string called 'NoRMaticConnectionString' in the connection strings
+        /// section of the config file.  However, an override can be applied here which will allow any requests for the current
+        /// connection string to be retrieved via a provider function.  This allows for runtime flexibility on how the connection
+        /// string is created.
+        /// </summary>
         public static void SetConnectionStringProvider(Func<string> provider) {
             ConfigContainer.Instance.ConnectionStringProvider = provider;
         }
