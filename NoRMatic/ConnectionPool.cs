4ceede334a53cd028530916c092e86f672804f9f
JC Grubbs <grubbs.jc@gmail.com>
2011-01-16 12:44:03 -0600

    ARGH

--- NoRMatic/ConnectionPool.cs
+++ NoRMatic/ConnectionPool.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using System.Threading;
 using Norm;
 
 namespace NoRMatic {
@@ -68,11 +69,13 @@ namespace NoRMatic {
 
         private void CheckContainersForInitialization(string connectionString) {
 
-            if (!_freeConnections.ContainsKey(connectionString))
-                _freeConnections[connectionString] = new Stack<ConnectionInfo>();
+            lock (_lock) {
+                if (!_freeConnections.ContainsKey(connectionString))
+                    _freeConnections[connectionString] = new Stack<ConnectionInfo>();
 
-            if (!_usedConnections.ContainsKey(connectionString))
-                _usedConnections[connectionString] = new List<ConnectionInfo>();
+                if (!_usedConnections.ContainsKey(connectionString))
+                    _usedConnections[connectionString] = new List<ConnectionInfo>();
+            }
         }
 
         private static ConnectionInfo CreateConnection(string connectionString) {
@@ -83,29 +86,38 @@ namespace NoRMatic {
         }
 
         private void Prune(string connectionString) {
-            for (var i = 0; i < _usedConnections[connectionString].Count; i++) {
-                var connectionInfo = _usedConnections[connectionString][i];
-                var lifetime = DateTime.Now.Ticks - connectionInfo.CreatedTimestamp;
-                if (connectionInfo.Connection.GetStream().DataAvailable || lifetime <= (MinTimeToLive * 10000)) continue;
-                ReclaimUsedConnectionToPool(connectionString, connectionInfo);
+            lock (_lock) {
+                for (var i = 0; i < _usedConnections[connectionString].Count; i++) {
+                    var connectionInfo = _usedConnections[connectionString][i];
+                    var lifetime = DateTime.Now.Ticks - connectionInfo.CreatedTimestamp;
+                    if (connectionInfo.Connection.GetStream().DataAvailable || lifetime <= (MinTimeToLive*10000))
+                        continue;
+                    ReclaimUsedConnectionToPool(connectionString, connectionInfo);
+                }
+                ReduceFreeConnectionsToBaseline(connectionString);
             }
-            ReduceFreeConnectionsToBaseline(connectionString);
         }
 
         private void ReduceFreeConnectionsToBaseline(string connectionString) {
-            while (_freeConnections[connectionString].Count > MaxFreeConnections) {
-                var connectionInfo = _freeConnections[connectionString].Pop();
-                DestroyConnection(connectionInfo.Connection);
+            lock (_lock) {
+                while (_freeConnections[connectionString].Count > MaxFreeConnections) {
+                    var connectionInfo = _freeConnections[connectionString].Pop();
+                    DestroyConnection(connectionInfo.Connection);
+                }
+                Monitor.Pulse(_lock);
             }
         }
 
         private void ReclaimUsedConnectionToPool(string connectionString, ConnectionInfo connectionInfo) {
-            _usedConnections[connectionString].Remove(connectionInfo);
-            if (connectionInfo.Connection.IsInvalid || !connectionInfo.Connection.IsConnected) {
-                DestroyConnection(connectionInfo.Connection);
-                return;
+            lock (_lock) {
+                _usedConnections[connectionString].Remove(connectionInfo);
+                if (connectionInfo.Connection.IsInvalid || !connectionInfo.Connection.IsConnected) {
+                    DestroyConnection(connectionInfo.Connection);
+                    return;
+                }
+                _freeConnections[connectionString].Push(connectionInfo);
+                Monitor.Pulse(_lock);
             }
-            _freeConnections[connectionString].Push(connectionInfo);
         }
 
         private static void DestroyConnection(IConnection connectionn) {
